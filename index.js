38cab8aae6a941d2d3d9275aa666cc3648eb6fed
maxwellbenton <maxwellbenton@gmail.com>
2019-02-07 10:20:07 -0500

    switches starter code to XHR example

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
-const testVar = {}
+document.addEventListener( 'DOMContentLoaded', () => {
+  getPosts()
+} )
 
-function testFunc() {
-  return "hi"
+const getPosts = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open( 'GET', 'http://localhost:3000/posts' );
+  xhr.addEventListener( 'load', e => {
+    JSON.parse( xhr.response )
+      .map( post => {
+
+        createPost( post )
+        post.comments.forEach( commentId => {
+
+          createEmptyComment( commentId, post.id )
+        } )
+      } )
+
+    getComments()
+  } )
+  xhr.send();
+}
+
+const createPost = post => {
+  let section = document.createElement( 'section' )
+  document.querySelector( 'main' )
+    .appendChild( section )
+  section.id = post.id
+  section.innerHTML = `<h2>${post.title}</h2><h3><img src=${post.user.image}>${post.user.name}</h3><p>${post.body}</p>`
+}
+
+const createEmptyComment = ( commentId, postId ) => {
+  let aside = document.createElement( 'aside' )
+  aside.id = commentId
+  document.getElementById( `${postId}` )
+    .appendChild( aside )
+}
+
+const getComments = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open( 'GET', `http://localhost:3000/comments` );
+  xhr.addEventListener( 'load', e => {
+    JSON.parse( xhr.response )
+      .map( comment => {
+        updateCommentContent( comment )
+      } )
+  } )
+  xhr.send();
+}
+
+const updateCommentContent = comment => {
+  let aside = document.getElementById( `${comment.id}` )
+  aside.innerHTML = `<h3><img src=${comment.user.image}>${comment.user.name}</h3><p>${comment.body}</p>`
 }
